105fe2718219cdd49c50bc9a1462041131c514fe
21stChrom <parkerjmartin2020@gmail.com>
2022-08-23 20:37:38 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
+import React from 'react';
 import './App.css';
-import StopWatch  from 'Components/StopWatch/StopWatch.js';
+import StopWatch from './Components/StopWatch/StopWatch.jsx';
 function App() {
   return (
     <div className="App">
